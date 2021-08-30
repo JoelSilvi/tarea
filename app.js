95bbab0d78854a8b999d41e84fa9c2f8ea61598b
JoelSilvi <analistasilvi@gmail.com>
2021-08-29 23:25:58 -0400

    completando más validaciones

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const btnInsertar = document.getElementById("insertar");
 const d = document;
+const btnInsertar = d.getElementById("insertar");
 
 const mensaje = (codigo, nombre, rut, edad, email) => {
   codigo = d.getElementById("cod").value;
@@ -7,18 +7,22 @@ const mensaje = (codigo, nombre, rut, edad, email) => {
   rut = d.getElementById("rut").value;
   edad = d.getElementById("edad").value;
   email = d.getElementById("email").value;
-  if (codigo === "") {
-    alert("el código no puede estar vacio");
+  if (codigo.length < 1) {
+    alert("código no puede estar vacío");
   } else if (codigo == 0) {
     alert("el código no puede ser cero");
-  } else if (nombre === "") {
-    alert("el nombre no puede estar vacío");
-  } else if (rut === "") {
-    alert("el rut no puede estar vacío");
+  } else if (nombre.length < 3) {
+    alert("nombre por lo menos debe contener 3 caracteres");
+  } else if (rut.length < 8) {
+    alert("el rut por lo menos debe tener 8 caracteres");
   } else if (edad === "") {
     alert("la edad no puede estar vacía");
-  } else {
-    alert("el email no puede estar vacío");
+  } else if (email.length < 7) {
+    alert("el email no puede contener menos de 7 caracteres");
+  } else if (!email.includes("@")) {
+    alert("el correo debe contener arroba");
+  } else if (!email.includes(".")) {
+    alert("el correo debe contener punto");
   }
 };
 btnInsertar.addEventListener("click", mensaje, true);
