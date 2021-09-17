deb1b270428cb462236fcd6ad0a153187b1e4bba
JoelSilvi <analistasilvi@gmail.com>
2021-09-17 02:53:58 -0300

    front listo

--- index.php
+++ index.php
@@ -3,17 +3,51 @@
 <head>
 	<title>Registrar usuario</title>
 	<meta charset="utf-8">
+  <link rel="stylesheet" href="app.css">
 </head>
 <body>
-    <form method="post">
-    	<h1>Registro Clientes</h1>
-      <input type="text" name="nomUsuario" placeholder="Nombre Usuario" required>
-    	<input type="text" name="pass" placeholder="Contraseña Usuario" required >
-      <input type="submit" name="crear" value="CREAR">
+  <h1>Registro Clientes</h1>
+    <fieldset>
+      <legend>Datos Cliente</legend> 
 
-    </form>
-        <?php 
-        include("crear.php");
-        ?>
+        <form method="post">	
+          <div class="codCliente">
+            <label>Código cliente:</label>
+            <input type="number" class="cod" name="cod"  >
+          </div>
+          
+          <div class="nomCliente">
+            <label>Nombre cliente:</label>
+            <input type="text" class="nom" name="name"  >
+          </div>
+
+          <div class="rutCliente">
+            <label>Rut cliente:</label>
+            <input type="text" class="rut" name="rut"  >
+          </div>
+
+          <div class="edadCliente">
+            <label>Edad del cliente:</label>
+            <input type="text" class="edad" name="edad" >
+          </div>
+
+          <div class="emailCliente">
+            <label>Correo electrónico cliente:</label>
+            <input type="email" class="email" name="email" >
+          </div>
+      <div class="botones">
+          <input type="submit" class="insertar" name="insertar" value="INSERTAR">
+          <input type="submit" class="delete" name="delete" value="ELIMINAR">
+          <input type="submit" class="update" name="update" value="ACTUALIZAR">
+        </div>
+
+      </form>
+    </fieldset>
+
+<?php 
+include("insertar.php");
+include("eliminar.php");
+include("update.php");
+?>
 </body>
 </html>
---
+++ index3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registrar usuario</title>
+	<meta charset="utf-8">
+</head>
+<body>
+    <form method="post">
+    	<h1>Registro Clientes</h1>
+      <input type="text" name="nomUsuario" placeholder="Nombre Usuario" required>
+    	<input type="text" name="pass" placeholder="Contraseña Usuario" required >
+      <input type="submit" name="crear" value="CREAR">
+
+    </form>
+        <?php 
+        include("crear.php");
+        ?>
+        
+</body>
+</html>
